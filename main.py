6bbd41cd99db2256f590f376350d56cff4c15173
VldmrShuklin <vshuklin13@gmail.com>
2024-03-21 21:42:19 +0400

    this code is getting more than 2 pics

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
-import time
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def download_image(url, filename):
@@ -10,24 +11,34 @@ def download_image(url, filename):
     with open(filename, 'wb') as file:
         file.write(response)
 
-
 def search_and_download(query, num_images):
-    if not os.path.exists('downloads'):
-        os.makedirs('downloads')
+    if not os.path.exists(f'downloads/{query}'):
+        os.makedirs(f'downloads/{query}')
 
     driver = webdriver.Firefox()
-    driver.get(f"https://www.google.com/search?q={query}&tbm=isch")
+    driver.get(f"https://www.google.com/search?q={query}&source=lnms&tbm=isch")
 
     images = driver.find_elements(By.XPATH, '//img[contains(@class,"rg_i")]')
 
-    for i, img in enumerate(images):
-        img.click()
-        time.sleep(2)
-        image_url = driver.find_element(By.CLASS_NAME, 'iPVvYb').get_attribute('src')
-        if image_url.startswith('data:image/'):
+    for i, img in enumerate(images[:num_images]):
+        try:
+            img.click()
+            image_src = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'iPVvYb'))
+            ).get_attribute('src')
+            if image_src.startswith("data:image/"):
+                continue
+            download_image(image_src, f"./downloads/{query}/{query}_{i}.jpg")
+
+            # Выполняет JavaScript код, который находит кнопку закрытия
+            # selenium не справился (я тоже)
+            driver.execute_script("document.querySelector('.uj1Jfd').click();")
+
+        except Exception as e:
+            print(e)
+            driver.execute_script("document.querySelector('.uj1Jfd').click();")
             continue
-        download_image(image_url, f"./downloads/{query}_{i}.jpg")
-        driver.find_element(By.CLASS_NAME, 'uj1Jfd').click()
+
     driver.quit()
 
 
