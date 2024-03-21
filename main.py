faa82fca9970860011b91464a9912adba9fc6fef
VldmrShuklin <vshuklin13@gmail.com>
2024-03-21 11:06:48 +0400

    refactor to selenium (still not working)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from bs4 import BeautifulSoup
 import requests
 import shutil
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 def download_image(url, filename):
     if url.startswith('data:image/'):
@@ -15,28 +16,14 @@ def download_image(url, filename):
             shutil.copyfileobj(response.raw, f)
 
 def search_and_download(query, num_images):
-    url = "https://www.google.com/search"
-    params = {
-        "q": query,
-        "tbm": "isch",
-        "ijn": "0"
-    }
-    headers = {
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-    }
+    driver = webdriver.Firefox()
+    driver.get(f"https://www.google.com/search?q={query}&tbm=isch")
 
-    response = requests.get(url, params=params, headers=headers)
+    images = driver.find_elements(By.XPATH, '//img[contains(@class,"rg_i")]')
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'lxml')
-        image_tags = soup.select('img.rg_i')
-        # print(soup.find_all('img', class_='rg_i'))
-        # print(f"{soup.select('img.rg_i')}")
-        for i, image_tag in enumerate(image_tags[:num_images]):
-            image_url = image_tag['src']
-            download_image(image_url, f"C:/projects/CarSideViewParser/downloads/{query}_{i}.jpg")
-    else:
-        print("Error:", response.status_code)
+    for i, img in enumerate(images[:num_images]):
+        image_url = img.get_attribute('src')
+        download_image(image_url, f"C:/projects/CarSideViewParser/downloads/{query}_{i}.jpg")
 
 search_query = input("Что: ")
 num_images_to_download = int(input("Сколько: "))
